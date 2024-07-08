ec055d5c36ab9b67e34a45a8168ded82427cf35c
nargizMakhanbetova <makhanbetovanargiz@gmail.com>
2024-07-08 20:46:24 +0600

    Add files via upload

---
+++ about.js
@@ -0,0 +1,3 @@
+// gsap.from(".p_about", { opacity: 0, duration: 1, y: 20, delay: 0.7 });
+// gsap.from(".navbar", { opacity: 0, duration: 1, y: 20 });
+// gsap.from(".burger_wrap", { opacity: 0, duration: 1, y: 20 });
---
+++ script.js
@@ -0,0 +1,20 @@
+let navbar = document.querySelector('.navbar')
+let burder = document.querySelector('.burder')
+let navlink = document.querySelectorAll('.navlink')
+burder.addEventListener('click', function(){
+    navbar.classList.toggle('nave')
+})
+// gsap.from(".navbar", { opacity: 0, duration: 1, y: 20 });
+// gsap.from(".burger_wrap", { opacity: 0, duration: 1, y: 20 });
+// gsap.from(".title", { opacity: 0, duration: 1, y: 20, delay: 0.5 });
+// gsap.from(".sticker", { opacity: 0, duration: 1, y: 20, delay: 0.7 });
+// gsap.from(".info_wrap", { opacity: 0, duration: 1, y: 20, delay: 0.7 });
+// gsap.from(".title_about", { opacity: 0, duration: 1, y: 20, delay: 0.5 });
+
+// gsap.from(".lopa", { opacity: 0, duration: 1, y: 20, delay: 0.9 });
+// gsap.from(".main_photo", { opacity: 0, duration: 1, y: 20, delay: 1.2 });
+// gsap.from(".sd", { opacity: 0, duration: 1, y: 20, delay: 0.9 });
+// gsap.from(".sd2", { opacity: 0, duration: 1, y: 20, delay: 1.2 });
+// gsap.from(".sd3", { opacity: 0, duration: 1, y: 20, delay: 1.4 });
+// gsap.from(".sd4", { opacity: 0, duration: 1, y: 20, delay: 1.6 });
+// gsap.from(".sd5", { opacity: 0, duration: 1, y: 20, delay: 1.8 });
